e9061bc11c4aea53e15e0b188f76f01a33dce454
SeongTae Jeong <kevin13@chol.net>
2019-06-05 23:26:36 +0900

    Add project files.

---
+++ WinForms/CopyRTFtoClipboard/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace CopyRTFtoClipboard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(12, 12);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(144, 23);
+            this.btnCopy.TabIndex = 0;
+            this.btnCopy.Text = "CopyToClipboard";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(12, 41);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(144, 23);
+            this.btnCheck.TabIndex = 1;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 175);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.btnCopy);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Button btnCheck;
+    }
+}
+
---
+++ WinForms/CopyRTFtoClipboard/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CopyRTFtoClipboard
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+//            string txt = @"{\rtf1\ansi\ansicpg949\deff0\nouicompat\deflang1033\deflangfe1042{\fonttbl{\f0\fnil\fcharset0 Malgun Gothic;}{\f1\fnil\fcharset129 Malgun Gothic;}{\f2\fnil\fcharset129 \'b8\'bc\'c0\'ba \'b0\'ed\'b5\'f1;}}
+//{\colortbl ;\red0\green0\blue0;\red0\green100\blue0;\red0\green0\blue255;}
+//{\*\generator Riched20 10.0.17763}\viewkind4\uc1 
+//\pard\cf1\f0\fs20\lang1042\par
+//\cf2 #\f1\'c5\'d7\'bd\'ba\'c6\'ae\'c0\'d4\'b4\'cf\'b4\'d92.\cf1\par
+//\par
+
+//\pard\sa200\sl276\slmult1\cf3 Get-Item\cf0\f2\lang18\par}
+//";
+
+            string txt = File.ReadAllText("data.txt");
+
+            DataObject dataObject = new DataObject();
+            dataObject.SetData(DataFormats.Rtf, txt);
+            Clipboard.SetDataObject(dataObject);
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            foreach (string txt in Clipboard.GetDataObject().GetFormats())
+            {
+                System.Diagnostics.Trace.WriteLine(txt);
+
+                object objValue = Clipboard.GetData(txt);
+                if (objValue != null)
+                {
+                    System.Diagnostics.Trace.WriteLine(objValue);
+
+                    WriteText(objValue as MemoryStream);
+                }
+            }
+        }
+
+        private void WriteText(MemoryStream ms)
+        {
+            if (ms == null)
+            {
+                return;
+            }
+
+            byte[] buf = new byte[ms.Length];
+            ms.Read(buf, 0, buf.Length);
+
+            string txt = Encoding.UTF8.GetString(buf);
+            System.Diagnostics.Trace.WriteLine(txt);
+        }
+    }
+}

a30d1094b495487f295e3648d3e33d5916728179
SeongTae Jeong <kevin13@chol.net>
2019-06-06 23:46:20 +0900

    Add CairoSharp (with gtk) sample project

---
+++ NetCore/CairoSharp_x64/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Cairo;
+
+namespace CairoSharp_x64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = 400;
+            int height = 400;
+
+            using (Surface surface = new ImageSurface(Cairo.Format.Rgb24, width, height))
+            {
+                surface.WriteToPng("plplot1.png");
+            }
+        }
+    }
+}

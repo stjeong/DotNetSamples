9d62be53de5215a461ed1481523b3a6f47277e56
SeongTae Jeong <kevin13@chol.net>
2019-06-10 00:04:15 +0900

    add cairo for windows forms(C#)

---
+++ PLPlotOutputToWindow/Form1.cs
@@ -0,0 +1,74 @@
+﻿using PLplot;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PLPlotOutputToWindow
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            if (File.Exists("PLplotNet.dll") == true)
+            {
+                File.Delete("PLplotNet.dll");
+            }
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Draw();
+        }
+
+        void Draw()
+        {
+            int width = 400;
+            int height = 400;
+            const int RGBsize = 3;
+
+            byte[] buffer = new byte[width * height * RGBsize];
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                buffer[i] = 0xff;
+            }
+
+            GCHandle handle = GCHandle.Alloc(buffer, GCHandleType.Pinned);
+            {
+                IntPtr pBuf = Marshal.UnsafeAddrOfPinnedArrayElement(buffer, 0);
+
+                using (var pl = new PLStream())
+                {
+                    pl.sdev("mem");
+                    pl.smem(width, height, pBuf);
+
+                    pl.init();
+
+                    pl.env(0, 10.0, 0, 10.0, 0, 0);
+                    pl.join(1.0, 2.0, 7.0, 8.0);
+                }
+
+                Bitmap bmp = new Bitmap(width, height, 3 * width, PixelFormat.Format24bppRgb, pBuf);
+                this.pictureBox1.Width = width;
+                this.pictureBox1.Height = height;
+                this.pictureBox1.Image = bmp;
+
+                // bmp.Save("test.bmp");
+            }
+            handle.Free();
+        }
+
+        private void drawPLplot_Click(object sender, EventArgs e)
+        {
+            Draw();
+        }
+    }
+}

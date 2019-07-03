c45712901b97cad87ea20f7c3e534366296a4736
SeongTae Jeong <kevin13@chol.net>
2019-07-03 22:27:25 +0900

    Add OpenCvSharp4 sample for .NET Core (Windows)

---
+++ NetCore/OpenCvSharp4Sample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using OpenCvSharp;
+
+namespace src
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mat src = new Mat("lenna.png", ImreadModes.Grayscale);
+            Mat dst = new Mat();
+
+            Cv2.Canny(src, dst, 50, 200);
+            using (new Window("src image", src))
+            using (new Window("dst image", dst))
+            {
+                Cv2.WaitKey();
+            }
+        }
+    }
+}

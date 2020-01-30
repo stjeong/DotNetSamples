2596d07f8702c06472818128e356db1cfa684c2c
SeongTae Jeong <kevin13@chol.net>
2020-01-31 01:55:41 +0900

    Add message loop for Console App

---
+++ WinConsole/UIThread/CustomMessageLoop/MessageLoop.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Threading;
+
+namespace CustomMessageLoop
+{
+    public class MessageLoop : IDisposable
+    {
+        uint _tid;
+        EventWaitHandle _ewh_Sync = new EventWaitHandle(true, EventResetMode.ManualReset);
+        EventWaitHandle _ewh_Exit = new EventWaitHandle(true, EventResetMode.ManualReset);
+
+        public ApartmentState COMApartment => _uiThread.GetApartmentState();
+
+        public void PostMessage(Win32Message msg)
+        {
+            PostMessage((uint)msg);
+        }
+
+        bool _disposed;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed == false)
+            {
+                if (disposing == true)
+                {
+                    SendMessage(Win32Message.WM_CLOSE);
+                    _ewh_Exit.WaitOne();
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public void PostMessage(uint msg)
+        {
+            if (_tid == 0)
+            {
+                return;
+            }
+
+            NativeMethods.PostThreadMessage(_tid, msg, UIntPtr.Zero, IntPtr.Zero);
+        }
+
+        public void WaitForExit(int millisecondsTimeout = Timeout.Infinite)
+        {
+            _ewh_Exit.WaitOne(millisecondsTimeout);
+        }
+
+        public void SendMessage(Win32Message msg)
+        {
+            SendMessage((uint)msg);
+        }
+
+        public void SendMessage(uint msg)
+        {
+            if (_tid == 0)
+            {
+                return;
+            }
+
+            _ewh_Sync.Reset();
+            NativeMethods.PostThreadMessage(_tid, msg, UIntPtr.Zero, IntPtr.Zero);
+            _ewh_Sync.WaitOne();
+        }
+
+        Thread _uiThread;
+        bool _useBackgroundThread;
+        ApartmentState _apartment;
+
+        public MessageLoop() : this(true, ApartmentState.STA) { }
+
+        public MessageLoop(bool useBackgroundThread) : this(useBackgroundThread, ApartmentState.STA) { }
+
+        public MessageLoop(bool useBackgroundThread, ApartmentState apartment)
+        {
+            _useBackgroundThread = useBackgroundThread;
+            _apartment = apartment;
+        }
+
+        public void Run()
+        {
+            _ewh_Exit.Set();
+
+            _uiThread = new Thread(Start);
+            _uiThread.SetApartmentState(_apartment);
+            _uiThread.IsBackground = _useBackgroundThread;
+            _uiThread.Start();
+        }
+
+        public event EventHandler Loaded;
+        public event EventHandler Closed;
+        public event EventHandler<MessageEventArgs> MessageArrived;
+
+        protected virtual void OnLoad() { }
+
+        protected virtual void OnClose() { }
+
+        protected virtual void WindowProc(MSG msg) { }
+
+        void Start()
+        {
+            _tid = NativeMethods.GetCurrentThreadId();
+
+            OnLoad();
+            Loaded?.Invoke(this, EventArgs.Empty);
+
+            try
+            {
+                while (true)
+                {
+                    int ret = NativeMethods.GetMessage(out MSG msg, IntPtr.Zero, 0, 0);
+                    if (ret == 0 || ret == -1)
+                    {
+                        break;
+                    }
+
+                    try
+                    {
+                        WindowProc(msg);
+                        MessageArrived?.Invoke(this, new MessageEventArgs(msg));
+
+                        switch (msg.message)
+                        {
+                            case (uint)Win32Message.WM_CLOSE:
+                                OnClose();
+                                Closed?.Invoke(this, EventArgs.Empty);
+                                return;
+                        }
+
+                        NativeMethods.TranslateMessage(ref msg);
+                        NativeMethods.DispatchMessage(ref msg);
+                    }
+                    finally
+                    {
+                        _ewh_Sync.Set();
+                    }
+                }
+            }
+            finally
+            {
+                _disposed = true;
+                _tid = 0;
+                _ewh_Exit.Set();
+            }
+        }
+    }
+}
---
+++ WinConsole/UIThread/CustomMessageLoop/NativeMethods.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace CustomMessageLoop
+{
+    public static class NativeMethods
+    {
+        [DllImport(@"user32.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+        internal static extern int GetMessage(out MSG message, IntPtr hWnd, uint wMsgFilterMin, uint wMsgFilterMax);
+        [DllImport(@"user32.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+        internal static extern bool TranslateMessage(ref MSG message);
+        [DllImport(@"user32.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+        internal static extern IntPtr DispatchMessage(ref MSG message);
+
+        [return: MarshalAs(UnmanagedType.Bool)]
+        [DllImport("user32.dll", SetLastError = true)]
+        internal static extern bool PostThreadMessage(uint threadId, uint msg, UIntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll")]
+        internal static extern uint GetCurrentThreadId();
+
+        [DllImport("user32.dll")]
+        internal static extern void PostQuitMessage(int nExitCode);
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POINT
+    {
+        public long x;
+        public long y;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct MSG
+    {
+        public IntPtr hwnd;
+        public uint message;
+        public IntPtr wParam;
+        public IntPtr lParam;
+        public uint time;
+        public POINT pt;
+    }
+
+    public enum Win32Message : uint
+    {
+        WM_CLOSE = 0x0010,
+        WM_USER = 0x0400,
+    }
+
+    public class MessageEventArgs : EventArgs
+    {
+        MSG _msg;
+        public MSG Message => _msg;
+
+        public MessageEventArgs(MSG msg)
+        {
+            _msg = msg;
+        }
+    }
+}
